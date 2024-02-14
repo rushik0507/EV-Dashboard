a5cb9be53f15ddb62c2459049a06fbff4c5f6d46
rushik0507 <rushikpateljnv@gmail.com>
2024-02-14 17:06:52 +0530

    mistake correction

--- src/components/fleetdata.jsx
+++ src/components/fleetdata.jsx
@@ -111,5 +111,27 @@ export const fleetData = {
         ],
       },
     },
+    {
+      vehicle_id: "EV001",
+      make: "Tesla",
+      model: "Model S",
+      metrics: {
+        kilometers_traveled: [
+          { date: "2024-01-01", value: 500 },
+          { date: "2024-01-02", value: 550 },
+          { date: "2024-01-03", value: 600 },
+        ],
+        carbon_emissions: [
+          { date: "2024-01-01", value: 20 },
+          { date: "2024-01-02", value: 25 },
+          { date: "2024-01-03", value: 30 },
+        ],
+        triptime: [
+          { date: "2024-01-01", value: 2 },
+          { date: "2024-01-02", value: 1 },
+          { date: "2024-01-03", value: 2 },
+        ],
+      },
+    },
   ],
 };
